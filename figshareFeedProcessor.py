5a796effea8bd48d906b31daf48a851eb2789563
Jim Tuttle <jim@braggtown.com>
2021-03-19 19:14:22 -0400

    Add get AWS input files, send email, put AWS output files

--- figshareFeedProcessor.py
+++ figshareFeedProcessor.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
 import xml.etree.ElementTree as ET
-import iconv   # iconv -f iso-8859-1 -t UTF-8 figdata.xml -o figdata2.xml
-import xmltodict
 from sys import exc_info
 import csv
 from datetime import date
-
+from requests import post
+from os import environ
+import boto3
 
 # Environmental variables
-is_dev = 0 # Set to non-zero when running locally for testing
-quota = "1073741824"  # default disk quota
 manualdata = "manualdata.xml"
 staffdata = "figdata.xml"
 studentdata = "student_export.csv"
 xmloutputfile = "hrfeed.xml"
 departmentsfile = "uniquedepartments.txt"
+# Figshare variables
+figshareurl = environ['FIGSHARE_API_URL']
+figsharetoken = environ['FIGSHARE_TOKEN']
+# Mailgun variables
+mailgunkey = environ['MAILGUN_KEY']
+mailgunurl = environ['MAILGUN_URL']
+emailrecipient = environ['RECIPIENT_EMAIL']
+emailsender = environ["SENDER_EMAIL"]
+# AWS variables
+awsid = environ["AWS_ACCESS_KEY_ID"]
+awskey = environ["AWS_SECRET_ACCESS_KEY"]
+
+
+def get_aws_files(keyid, accesskey, manualfile, stafffile, studentfile):
+    s3 = boto3.client("s3", aws_access_key_id=keyid, aws_secret_access_key=accesskey)
+    s3.download_file("vtlib-figshare-hrdata", stafffile, stafffile)
+    s3.download_file("vtlib-figshare-studentdata", studentfile, studentfile)
+    # Where is the manual file?
 
 
 def process_manual_data(manualfile):
     try:
         records = []
-        with open(manualfile, "r",) as f:
+        with open(manualfile, "r", ) as f:
             data = f.read().replace("\n", "")
         root = ET.fromstring(data)
         for child in root:
@@ -110,6 +126,7 @@ def find_unique_departments(records):
     adddepartments.sort()
     with open(departmentsfile, mode="wt", encoding="utf-8") as f:
         f.write("\n".join(newdepartements))
+    return olddepartments, newdepartements, adddepartments, removedepartments
 
 
 def create_xml_output(records):
@@ -120,7 +137,7 @@ def create_xml_output(records):
             if key == "IsCurrent":
                 outxml += "<IsCurrent>Y</IsCurrent>\n"
             elif (key == "PrimaryGroupDescriptor") or (key == "Username"):
-                pass # Figshare isn't using these elements
+                pass  # Figshare isn't using these elements
             else:
                 outxml += "<%s>%s</%s>\n" % (key, value, key)
         if "Quota" not in record.keys():
@@ -133,37 +150,59 @@ def create_xml_output(records):
     return outxml
 
 
-def build_message(manualrecs, staffrecs, studentrecs, dedupilcatedrecs, ):
+def build_message(manualrecs, staffrecs, studentrecs, dedupilcatedrecs, olddepartments, newdepartements, adddepartments,
+                  removedepartments):
     message = "Figshare Feed Processor Report: %s \n\n" % (date.today().strftime("%Y-%m-%d"))
-    message += "The following changes have been discovered in the HR Feed department field:/n"
+    message += "Found %s manual records, %s staff records, and %s student records for total of %s records. %s records " \
+               "were used after deduplication.\n" % (len(manualrecs), len(staffrecs), len(studentrecs),
+                                                     str(len(manualrecs) + len(staffrecs) + len(studentrecs)),
+                                                     len(dedupilcatedrecs))
+    message += "This feed contained %s unique departments.  There were %s unique departments in the most recent feed.\n\n" \
+               % (len(newdepartements), len(olddepartments))
+    if len(adddepartments) > 0:
+        message += "The following departments have been removed:\n"
+        for d in adddepartments:
+            message += "%s \n" % d
+    else:
+        message += "No departments were removed.\n"
+    if len(adddepartments) > 0:
+        message += "The following departments have been added:\n"
+        for d in removedepartments:
+            message += "%s \n" % d
+    else:
+        message += "No departments were added.\n"
+    return message
 
 
-def send_email(adddepts, removedepts):
-    if len(adddepts) > 0:
-        message += "Departments that were newly discovered in the HR Feed:/n"
-        for d in adddepts:
-            message+= "/t {} /n".format(d)
-        message += "/n"
-    if len(removedepts) > 0:
-        message += "Departments that no longer exist in the HR Feed:/n"
-        for d in removedepts:
-            message += "/t {} /n".format(d)
-        message += "/n"
-    if not is_dev:
-        with open("uniqdeptmsg.txt", "w") as uniqd:
-            uniqd.write(message)
+def put_aws_files(keyid, accesskey, xmlfile, deptsfile):
+    s3 = boto3.client("s3", aws_access_key_id=keyid, aws_secret_access_key=accesskey)
+    s3.upload_file(xmlfile, "vtlib-figshare-hrdata", xmlfile)
+    s3.upload_file(deptsfile, "vtlib-figshare-hrdata", deptsfile)
+
+
+def send_message(key, url, sender, recipient, body):
+    request_url = url.format()
+    subject = "Figshare Feed Processor Report: %s" % (date.today().strftime("%Y-%m-%d"))
+    response = post(request_url, auth=('api', key), data={
+        "from": sender,
+        "to": "james.tuttle@vt.edu",  # recipient,
+        "subject": subject,
+        "text": body
+    })
+    print('Status: {0}'.format(response.status_code))
+    print('Body:   {0}'.format(response.text))
 
 
 if __name__ == "__main__":
+    get_aws_files(awsid, awskey, manualdata, staffdata, studentdata)
     manualrecords = process_manual_data(manualdata)
     staffrecords = process_staff_data(staffdata)
     studentrecords = process_student_data(studentdata)
-    dedupilcatedrecords= dedup_records(manualrecords, staffrecords, studentrecords)
-    find_unique_departments(dedupilcatedrecords)
+    dedupilcatedrecords = dedup_records(manualrecords, staffrecords, studentrecords)
+    olddepts, newdepts, adddepts, removedepts = find_unique_departments(dedupilcatedrecords)
     xmloutput = create_xml_output(dedupilcatedrecords)
-
-
-
-
-
-
+    messagebody = build_message(manualrecords, staffrecords, studentrecords, dedupilcatedrecords, olddepts, newdepts,
+                                adddepts, removedepts)
+    put_aws_files(awsid, awskey, xmloutputfile, departmentsfile)
+    send_message(mailgunkey, mailgunurl, emailsender, emailrecipient, messagebody)
+    print("done")
